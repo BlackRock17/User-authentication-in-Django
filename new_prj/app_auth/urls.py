07cc4595419df81f2a8b7aa5a7dfabbd827dfe50
Lyubomir <119952676+BlackRock17@users.noreply.github.com>
2023-10-19 15:04:31 +0300

    test comment

--- new_prj/app_auth/urls.py
+++ new_prj/app_auth/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from new_prj.app_auth.views import RegisterUserView, LoginUserView, LogoutUserView
+
+urlpatterns = (
+    path('register/', RegisterUserView.as_view(), name='register_user'),
+    path('login/', LoginUserView.as_view(), name='login_user'),
+    path('logout/', LogoutUserView.as_view(), name='logout_user'),
+)
--- new_prj/app_auth/views.py
+++ new_prj/app_auth/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import forms as auth_forms
+from django.views import generic as views
 from django.shortcuts import render
 
-# Create your views here.
+
+class RegisterUserView(views.CreateView):
+    template_name = 'app_auth/register.html'
+    form_class = auth_forms.UserCreationForm
+
+
+class LoginUserView(views.View):
+    pass
+
+
+class LogoutUserView(views.View):
+    pass
